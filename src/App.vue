96385bab192c241e76d898afa9bb3eb9a883349a
unit-studio <unitstudio@163.com>
2021-03-11 19:03:11 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <Map></Map>
+  </div>
+</template>
+
+<script>
+import Map from './components/Map.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Map
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 0px;
+  margin: 0;
+}
+</style>
---
+++ src/components/Map.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="box" id="box"></div>
+</template>
+
+<script>
+import * as echarts from 'echarts';
+
+export default {
+  data() {
+    return {
+      chart: {},
+      options: {},
+    };
+  },
+  mounted() {
+    this.chart = echarts.init(document.getElementById('box'))
+
+    this.option = {
+      color: ['red', 'green', 'yellow'],
+      title: {
+          text: 'ECharts 入门示例'
+      },
+      legend: {
+        data: ['销量','收入']
+      },
+      tooltip: {},
+      xAxis: {
+          type: 'category',
+          boundaryGap: true,
+          data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [{
+          name: '销量',
+          type: 'line',
+          stack: 'total',
+          data: [5, 20, 36, 10, 10, 20]
+      },{
+          name: '收入',
+          type: 'line',
+          stack: 'total',
+          data: [5, 20, 36, 10, 10, 20]
+      }]
+    };
+
+    this.chart.setOption(this.option)
+
+  }
+}
+</script>
+
+<style lang="css" scoped>
+#box {
+  width: 600px;
+  height: 400px;
+  margin: 10px auto;
+}
+</style>
